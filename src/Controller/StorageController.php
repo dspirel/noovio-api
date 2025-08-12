d7384bc5ac5efee196ca880c6adc8dbb3371078f
Davor Spirelja <davor.spirelja@gmail.com>
2025-08-12 12:19:33 +0200

    init

---
+++ src/Controller/StorageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\GoogleCloudStorageService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+
+use function Symfony\Component\Clock\now;
+
+final class StorageController extends AbstractController
+{
+    #[Route('/storage/getUserFolders', name: 'app_user_folders')]
+    public function getUserFolders(
+        GoogleCloudStorageService $gcs,
+        Request $request
+        ): JsonResponse
+        {
+            $data = json_decode($request->getContent(), true);
+
+            $apiSecretKey = $_ENV['API_SECRET_KEY'] ?? null;
+            if ($data['secret'] !== $apiSecretKey) {
+                return $this->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+            }
+
+            $folders = $gcs->listUserFolders('StudioVision');
+
+            return new JsonResponse([
+                'data' => $folders
+            ], Response::HTTP_OK);
+        }
+
+        #[Route('/', name: 'app_home')]
+        public function home(): JsonResponse
+        {
+            return $this->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        }
+
+}
---
+++ src/Service/GoogleCloudStorageService.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Service;
+
+use Google\Cloud\Storage\StorageClient;
+use Google\Cloud\Storage\StorageObject;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use function Symfony\Component\Clock\now;
+
+
+class GoogleCloudStorageService
+{
+    private StorageClient $storage;
+    private string $mainBucket = 'gcstorage-n8n';
+
+    public function __construct(StorageClient $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    public function listUserFolders(string $userFolder): array
+    {
+        $bucket = $this->storage->bucket($this->mainBucket);
+
+        $prefix = rtrim($userFolder, '/') . '/';
+
+        $options = [
+            'prefix' => $prefix,
+            'delimiter' => '.'
+        ];
+
+        $results = $bucket->objects($options);
+        $folders = [];
+
+        foreach ($results as $folder) {
+            $f = str_replace($prefix, '', $folder->name());
+            $f = rtrim($f, '/');
+            if ($f !== '') $folders[] = $f;
+        }
+
+        return $folders;
+    }
+    // public function listUserFiles(string $userId): array
+    // {
+    //     $bucket = $this->storage->bucket($this->bucketName);
+    //     $objects = $bucket->objects(['prefix' => "$userId/"]);
+    //     $urlExpiration = now('+30 minutes');
+
+    //     $files = [];
+    //     foreach ($objects as $object) {
+    //         $files[] = [
+    //             'name' => $object->name(),
+    //             'url' => $this->getSignedUrl($object->name(), $urlExpiration),
+    //         ];
+    //     }
+
+    //     return $files;
+    // }
+
+    // public function listUserImages(string $userId): array
+    // {
+    //     $bucket = $this->storage->bucket($this->bucketName);
+    //     $objects = $bucket->objects(['prefix' => "$userId/"]);
+    //     $urlExpiration = now('+30 minutes');
+
+    //     $files = [];
+    //     foreach ($objects as $object) {
+    //         if (str_ends_with(strtolower($object->name()), '.jpg') ||
+    //             str_ends_with(strtolower($object->name()), '.jpeg')) {
+    //             $files[] = [
+    //                 'name' => $object->name(),
+    //                 'url' => $this->getSignedUrl($object->name(), $urlExpiration),
+    //                 ];
+    //             }
+    //     }
+
+    //     return $files;
+    // }
+
+    // public function deleteFile(string $filePath): void
+    // {
+    //     $bucket = $this->storage->bucket($this->bucketName);
+    //     $object = $bucket->object($filePath);
+
+    //     if ($object->exists()) {
+    //         $object->delete();
+    //     }
+    // }
+
+    // /**
+    //  * List files in a Google Cloud Storage bucket, optionally filtered by extension.
+    //  *
+    //  * @param string $bucketName The name of the bucket
+    //  * @param string $folder The folder path (e.g., 'uploads/') to filter by
+    //  * @param string $extension The file extension to filter by (e.g., 'jpg')
+    //  * @param array $options Additional options (e.g., prefix, delimiter)
+    //  * @return array List of file names
+    //  * @throws \RuntimeException If listing fails
+    //  */
+    // public function listFilesWithExtension(string $folder, string $extension): array
+    // {
+    //     try {
+    //         $options = [];
+    //         $bucket = $this->storage->bucket($this->bucketName);
+    //         // if (!$bucket->exists()) {
+    //         //     throw new \RuntimeException(sprintf('Bucket "%s" does not exist.', $bucketName));
+    //         // }
+
+    //         // Set prefix to filter by folder if provided
+    //         if ($folder !== null) {
+    //             $options['prefix'] = rtrim($folder, '/') . '/';
+    //         }
+
+    //         // Set delimiter to limit results to the specified folder
+    //         $options['delimiter'] = '/';
+
+
+    //         $objects = $bucket->objects($options);
+    //         $fileNames = [];
+
+    //         foreach ($objects as $object) {
+    //             $fileName = $object->name();
+
+    //             // Filter by extension if specified
+    //             if ($extension !== null && !str_ends_with(strtolower($fileName), '.' . strtolower($extension))) {
+    //                 continue;
+    //             }
+
+    //             $fileNames[] = $fileName;
+    //         }
+
+    //         return $fileNames;
+    //     } catch (\Exception $e) {
+    //         throw new \RuntimeException(sprintf('Failed to list files: %s', $e->getMessage()), 0, $e);
+    //     }
+    // }
+
+    // /**
+    //  * Get a signed URL for accessing a file.
+    //  *
+    //  * @param string $bucketName The name of the bucket
+    //  * @param string $objectName The path to the file in the bucket
+    //  * @param \DateTimeInterface|int $expires Expiration time (DateTime or seconds from now)
+    //  * @param array $options Additional options for the signed URL
+    //  * @return string The signed URL
+    //  * @throws \RuntimeException If URL generation fails
+    //  */
+    // public function getSignedUrl(string $objectName, $expires, array $options = []): string
+    // {
+    //     try {
+    //         $bucket = $this->storage->bucket($this->bucketName);
+    //         $object = $bucket->object($objectName);
+
+    //         if (!$object->exists()) {
+    //             throw new NotFoundHttpException(sprintf('File "%s" does not exist in bucket "%s".', $objectName, $this->bucketName));
+    //         }
+
+    //         $url = $object->signedUrl($expires, $options);
+
+    //         return $url;
+    //     } catch (\Exception $e) {
+    //         throw new \RuntimeException(sprintf('Failed to generate signed URL: %s', $e->getMessage()), 0, $e);
+    //     }
+    // }
+
+    // public function uploadFile(string $localPath, string $destinationPath, bool $public = false): string
+    // {
+    //     $bucket = $this->storage->bucket($this->bucketName);
+
+    //     $options = ['name' => $destinationPath];
+
+    //     if ($public) {
+    //         $options['predefinedAcl'] = 'publicRead';
+    //     }
+
+    //     $object = $bucket->upload(fopen($localPath, 'r'), $options);
+
+    //     return sprintf('https://storage.googleapis.com/%s/%s', $this->bucketName, $destinationPath);
+    // }
+}
